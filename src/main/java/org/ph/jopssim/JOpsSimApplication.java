7ce17b1d7814252ff1f00ea4da7f0669a38635bc
pierre-hugues.charbo <pierre-hugues.charbo@CA-LHDWKNF2.groupinfra.com>
2019-11-17 13:49:33 -0500

    Extra logging

--- src/main/java/org/ph/jopssim/JOpsSimApplication.java
+++ src/main/java/org/ph/jopssim/JOpsSimApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JOpsSimApplication {
 	
 	public static void main(String[] args) {
+		System.out.println("Hello PH!!");
+		
 		SpringApplication.run(JOpsSimApplication.class, args);
 	}
 }
