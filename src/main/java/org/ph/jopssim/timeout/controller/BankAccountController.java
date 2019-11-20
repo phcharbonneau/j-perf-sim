0f3a5573c242908a119ca274944032a65dc7d945
pierre-hugues.charbo <pierre-hugues.charbo@CA-LHDWKNF2.groupinfra.com>
2019-11-20 09:19:02 -0500

    BankAccountController

--- src/main/java/org/ph/jopssim/timeout/controller/BankAccountController.java
+++ src/main/java/org/ph/jopssim/timeout/controller/BankAccountController.java
@@ -1,5 +1,7 @@
 package org.ph.jopssim.timeout.controller;
 
+import java.util.Random;
+
 import org.ph.jopssim.timeout.model.BankAccount;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +13,16 @@ public class BankAccountController {
 	@RequestMapping("/banknb")
     public long banknb(@RequestParam(value="userID", defaultValue="PH") String userID) {
 		
+		int max = 15000;
+		int min = 100;
+		Random rn = new Random();
+		
+		int responTime = rn.nextInt(max - min + 1) + min;
+		
+		System.out.println("RESP TIME!! "+responTime);
+		
 		try {
-			Thread.sleep(10000);
+			Thread.sleep(responTime);
 		} catch (InterruptedException e) {}
 		
 		// return the current Bank account Nb
