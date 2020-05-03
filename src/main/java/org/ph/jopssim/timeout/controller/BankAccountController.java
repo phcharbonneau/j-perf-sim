8a37133c079d124df8741f26883433f0339b039c
phcha <phcha@host.docker.internal>
2020-05-03 13:52:10 -0400

    Cleanup

--- src/main/java/org/ph/jopssim/timeout/controller/BankAccountController.java
+++ src/main/java/org/ph/jopssim/timeout/controller/BankAccountController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * BankAccountController
+ * @author phcha
+ *
+ */
 @RestController
 public class BankAccountController {
 	
 	@RequestMapping("/banknb")
     public long banknb(@RequestParam(value="userID", defaultValue="PH") String userID) {
 		
+		// random response time between 100ms - 15 secs
 		int max = 15000;
 		int min = 100;
 		Random rn = new Random();
@@ -26,6 +32,6 @@ public class BankAccountController {
 		} catch (InterruptedException e) {}
 		
 		// return the current Bank account Nb
-		return new Long(System.currentTimeMillis());
+		return System.currentTimeMillis();
     }
 }
--- src/main/java/org/ph/jopssim/timeout/controller/MyAccountController.java
+++ src/main/java/org/ph/jopssim/timeout/controller/MyAccountController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * MyAccountController
+ * @author phcha
+ *
+ */
 @RestController
 public class MyAccountController {
 	
